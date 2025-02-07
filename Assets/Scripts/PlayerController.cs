ed95537ab30fbc782fc31a6fbbbe28eea55dec2b
pacapara <rachel_monteiro@fitnyc.edu>
2025-02-06 19:46:36 -0500

    InputSystem setup
    Setup input system, jump and left/right movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float input_xMovement;
+    public bool input_Jump;
+
+    private InputActionMap inputActions;
     // Start is called before the first frame update
     void Start()
     {
-        
+       // inputActions = GetComponent<InputActionMap>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Inputs_Movement(InputAction.CallbackContext ctx)
+    {
+         input_xMovement = ctx.ReadValue<float>();  //-1 for left, 1 for right. 
+    }
+
+    public void Imputs_Jump(InputAction.CallbackContext ctx)
+    {
+        if(ctx.started) { input_Jump = true;}
+        if(ctx.canceled) { input_Jump = false;}
     }
 }
