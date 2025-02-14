26c1d6e0964f2dbcd99859819c451ebe81a571c9
pacapara <rachel_monteiro@fitnyc.edu>
2025-02-13 20:58:44 -0500

    Jump
    Camera added, jump added. Jump sucks and doesnt work, issue with input detection

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,20 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerController playerController;
+    private Rigidbody playerRb;
+    private Vector3 playerMove;
 
-    private Vector3 playerPos;
+    private bool isGrounded;
+    private Vector3 jump;
+    private float jumpForce = 100f;
 
     void Start()
     {
         playerController = GameObject.FindWithTag("PlayerController").GetComponent<PlayerController>();
-        playerPos = transform.position;
+        playerRb = GetComponent<Rigidbody>();
+        playerMove = new Vector3 (0.5f, 0f,0f);
+        
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
 
     // Update is called once per frame
@@ -23,15 +30,29 @@ public class PlayerMovement : MonoBehaviour
 
     void MovementManager()
     {
-        transform.position = playerPos;
+        MoveLeftRight();
+        MoveJump();
 
+    }
+
+    void MoveLeftRight()
+    {
         if (playerController.input_xMovement < 0)
         {
-            playerPos.x += -1f;
+            transform.position -= playerMove;
         }
-        else if (playerController.input_xMovement > 0) 
+        else if (playerController.input_xMovement > 0)
         {
-            playerPos.x += 1f;
+            transform.position += playerMove;  
+        }
+    }
+
+    void MoveJump()
+    {
+       
+            if (playerController.input_Jump)
+        {
+            playerRb.AddForce(jump * jumpForce * Time.deltaTime, ForceMode.Impulse);
         }
     }
 }
