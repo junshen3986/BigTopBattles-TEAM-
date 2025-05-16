a5c089156e6e6545cdfcd70b662c320b94ccb0fb
junshen3986 <junshen_gao@fitnyc.edu>
2025-05-16 01:49:45 -0400

    A

---
+++ Assets/Animation/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Animator animator;
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(h * moveSpeed * Time.deltaTime, 0f, 0f);
+        animator.SetBool("IsWalking", h != 0f);
+
+        Debug.Log("X position: " + transform.position.x);
+    }
+}
